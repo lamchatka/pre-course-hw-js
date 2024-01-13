143d2b590682a53ad72abfd98691dbfb487aee25
Maxim Rukoleev <maxprolife@icloud.com>
2024-01-13 16:03:53 +0300

    ex 5

---
+++ lesson-02/05.js
@@ -0,0 +1,12 @@
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+
+let passport2 = {
+    name: "Ivan",
+    surname: "Petrov",
+};
+
+console.log(passport);
+console.log(passport2);
